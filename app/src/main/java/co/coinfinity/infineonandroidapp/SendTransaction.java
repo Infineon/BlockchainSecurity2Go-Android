9fe8a18868df1cc850f096ae74fdd76ec0e7ecf6
Daniel Schweiger <daniel.schweiger@coinfinity.co>
2018-08-20 14:30:34 +0200

    add qr code scanner for reading ether address

--- app/src/main/java/co/coinfinity/infineonandroidapp/SendTransaction.java
+++ app/src/main/java/co/coinfinity/infineonandroidapp/SendTransaction.java
@@ -2,6 +2,7 @@ package co.coinfinity.infineonandroidapp;
 
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.net.Uri;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
 import android.os.Bundle;
@@ -93,5 +94,36 @@ public class SendTransaction extends AppCompatActivity {
 
         thread.start();
         finish();
+    }
+
+    public void scanQrCode(View view) {
+        try {
+
+            Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+            intent.putExtra("SCAN_MODE", "QR_CODE_MODE"); // "PRODUCT_MODE for bar codes
+
+            startActivityForResult(intent, 0);
+
+        } catch (Exception e) {
+
+            Uri marketUri = Uri.parse("market://details?id=com.google.zxing.client.android");
+            Intent marketIntent = new Intent(Intent.ACTION_VIEW, marketUri);
+            startActivity(marketIntent);
+
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 0) {
+
+            if (resultCode == RESULT_OK) {
+                recipientAddressTxt.setText(data.getStringExtra("SCAN_RESULT"));
+            }
+            if (resultCode == RESULT_CANCELED) {
+                //handle cancel
+            }
+        }
     }
 }
